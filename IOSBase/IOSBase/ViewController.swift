c3b2a4b5c25f122e73f4209f9281bbb03dac3b77
anhtdt <anhtdt@vnpay.vn>
2023-05-05 09:56:42 +0700

    init project

---
+++ IOSBase/IOSBase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOSBase
+//
+//  Created by Tran Duc Tien Anh on 05/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
